a11f8fbf54a87709b3993b480607788f2fe6dd42
xmov <xiiiovart@gmail.com>
2025-08-09 10:46:14 +0300

    pivot_tables

--- scripts/pivot_tables.py
+++ scripts/pivot_tables.py
@@ -18,3 +18,23 @@ def get_median_for_category(df):
 print(get_max(df))
 print(get_max_for_category(df))
 print(get_median_for_category(df))
+
+def calculate_retention(csv_data):
+  df = pd.read_csv(csv_data)
+  df['FirstVisitMonth'] = pd.to_datetime(df['FirstVisitDate']).dt.to_period('M')
+  df['FollowUpVisitMonth'] = pd.to_datetime(df['FollowUpVisitDate']).dt.to_period('M')
+
+  cohort_data = df.groupby(['FirstVisitMonth', 'FollowUpVisitMonth']).agg({'UserID': 'nunique'}).reset_index()
+  cohort_counts = cohort_data.pivot_table(index='FirstVisitMonth', columns='FollowUpVisitMonth', values='UserID')
+
+  for row in cohort_counts.index:
+    first_non_zero = cohort_counts.loc[row].first_valid_index()
+    cohort_counts.loc[row] = (cohort_counts.loc[row] / cohort_counts.loc[row, first_non_zero]).round(2).fillna(0)
+
+  return cohort_counts
+
+print(calculate_retention('./datas/visits.csv'))
+
+# df = calculate_retention('./datas/visits.csv')
+# df[:'2021-01'] = df.loc['2021-01'] + 10
+# print(df)
